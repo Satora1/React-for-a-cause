45d02c74bdd23f7d1d5c62882829fbdf1c797076
Satora1 <satorakuba@gmail.com>
2023-03-01 11:11:58 +0100

    change

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,36 +1,53 @@
-import { useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Form() {
-  const DIV = `<h1><div>
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name">
-  </div>
-  <div>
-    <label for="surname">Surname:</label>
-    <input type="text" id="surname" name="surname">
-  </div>
-  <div>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email">
-  </div></h1>
-
-  <button class="Send">
- Send
-  </button>
-  `;
+  const DIV = `<div class="sheath">
+  <h1>Make a Donation</h1>
+  <form>
+    <div class="form-group">
+      <label for="name">Name/Surname:</label>
+      <input type="text" id="name" name="name" required>
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+      <label for="donation">Donation Amount:</label>
+      <div class="radio-group">
+        <input type="radio" id="donation10" name="donation" value="10" required>
+        <label for="donation10">$10</label>
+      </div>
+      <div class="radio-group">
+        <input type="radio" id="donation25" name="donation" value="25" required>
+        <label for="donation25">$25</label>
+      </div>
+      <div class="radio-group">
+        <input type="radio" id="donation50" name="donation" value="50" required>
+        <label for="donation50">$50</label>
+      </div>
+    </div>
+    <input type="submit" value="Submit" id="submit">
+  </form>
+</div>`;
 
   const [content, setContent] = useState('Click me!');
   const [displayInputs, setDisplayInputs] = useState(false);
   const sheathRef = useRef(null);
 
-
   const handleClick = () => {
     setDisplayInputs(true);
     setContent(DIV);
-
   };
 
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Form submitted!');
+    setDisplayInputs(false);
+    setContent('Click me!');
+    console.log('thank you');
+  };
+
   const handleOutsideClick = (event) => {
     if (sheathRef.current && !sheathRef.current.contains(event.target)) {
       setDisplayInputs(false);
@@ -39,22 +56,20 @@ function Form() {
   };
 
 
-
-
   useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener('mousedown', handleOutsideClick);
     return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, []);
 
-
-  
   return (
     <div>
       {displayInputs ? (
         <div ref={sheathRef} className="sheath">
-          <div dangerouslySetInnerHTML={{ __html: content }}></div>
+          <form onSubmit={handleSubmit}>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+          </form>
         </div>
       ) : (
         <div onClick={handleClick}>
