b816418f5dfa886c990cfa8886e1fd68835d46c0
Agata Gruszka <grusia321@gmail.com>
2023-03-01 09:27:24 +0100

    Welcome section & about us

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Welcome from './components/Welcome';
+import AboutUs from './components/AboutUs';
+
+import HowToHelp from './components/HowToHelp';
+
+import { useState } from 'react';
+import Contact from './components/Contact';
+
+
+
 function App() {
+  const title = "Future belongs to us!"
+  const shortDesc = "You have the power, you have the influence - help us make the future brighter"
+  const [aboutUsIsShown, SetAboutUsVisibility] = useState(false);
+  const [contactIsShown, SetContactVisibility] = useState(false);
+  const [homeIsShown, SetHomeVisibility] = useState(true);
+  const [helpIsShown, SetHelpVisibility] = useState(false);
+
+
+  function handleClickOnAboutUs() {
+    if (aboutUsIsShown) {
+      SetAboutUsVisibility(false)
+      SetHomeVisibility(true)
+    }
+    else {
+      SetAboutUsVisibility(true)
+      SetHomeVisibility(false)
+      SetContactVisibility(false)
+      SetHelpVisibility(false)
+    }
+  }
+
+  function handleClickOnContact() {
+    if (contactIsShown) {
+      SetContactVisibility(false)
+      SetHomeVisibility(true)
+    }
+    else {
+      SetContactVisibility(true)
+      SetHomeVisibility(false)
+      SetAboutUsVisibility(false)
+      SetHelpVisibility(false)
+    }
+  }
+
+  function handleClickOnHelp() {
+    if (helpIsShown) {
+      SetHelpVisibility(false)
+      SetHomeVisibility(true)
+    }
+    else {
+      SetHelpVisibility(true)
+      SetContactVisibility(false)
+      SetHomeVisibility(false)
+      SetAboutUsVisibility(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="nav">
+        <button onClick={handleClickOnAboutUs}>About us</button>
+        <button onClick={handleClickOnHelp} >How to help?</button>
+        <button onClick={handleClickOnContact} >Contact</button>
+      </div>
+      {homeIsShown && <Welcome title={title} shortDesc={shortDesc} />}
+      {aboutUsIsShown && <AboutUs></AboutUs>}
+      {contactIsShown && <Contact></Contact>}
+      {helpIsShown && <HowToHelp></HowToHelp>}
     </div>
   );
 }
