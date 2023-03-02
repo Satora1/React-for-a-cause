88c436ec5931b8c71fe9978c8d11332e488240f1
lukaszdutka <lukaszdutka2@gmail.com>
2023-03-02 11:12:56 +0100

    post review changes - refactor visibility into one state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "./components/ChildrenHolEduc.css";
-import { useState } from "react";
+import {useState} from "react";
 import PureModal from 'react-pure-modal';
 import 'react-pure-modal/dist/react-pure-modal.min.css';
 import Contact from './components/Contact';
@@ -8,86 +8,81 @@ import Welcome from './components/Welcome';
 import AboutUs from './components/AboutUs';
 import HowToHelp from './components/HowToHelp';
 import Form from './components/Form';
+import ChildrenCard from "./components/ChildrenCard";
 
 function App() {
-
-  const shortDesc = "You have the power, you have the influence - help us make the future brighter"
-  const title = "Future belongs to us!"
-  const [modal, setModal] = useState(false);
-  const [aboutUsIsShown, SetAboutUsVisibility] = useState(false);
-  const [contactIsShown, SetContactVisibility] = useState(false);
-  const [homeIsShown, SetHomeVisibility] = useState(true);
-  const [helpIsShown, SetHelpVisibility] = useState(false);
-
-  function handleClickOnAboutUs() {
-    if (aboutUsIsShown) {
-      SetAboutUsVisibility(false)
-      SetHomeVisibility(true)
+    const nothing = {
+        home: false,
+        aboutUs: false,
+        contact: false,
+        help: false,
+        modal: false,
+        children: false
     }
-    else {
-      SetAboutUsVisibility(true)
-      SetHomeVisibility(false)
-      SetContactVisibility(false)
-      SetHelpVisibility(false)
-    }
-  }
 
-  function handleClickOnContact() {
-    if (contactIsShown) {
-      SetContactVisibility(false)
-      SetHomeVisibility(true)
-    }
-    else {
-      SetContactVisibility(true)
-      SetHomeVisibility(false)
-      SetAboutUsVisibility(false)
-      SetHelpVisibility(false)
-      setModal(true)
-    }
-  }
+    const shortDesc = "You have the power, you have the influence - help us make the future brighter"
+    const title = "Future belongs to us!"
 
-  function handleClickOnHelp() {
-    if (helpIsShown) {
-      SetHelpVisibility(false)
-      SetHomeVisibility(true)
-    }
-    else {
-      SetHelpVisibility(true)
-      SetContactVisibility(false)
-      SetHomeVisibility(false)
-      SetAboutUsVisibility(false)
-    }
-  }
+    const [visibility, setVisibility] = useState({
+        ...nothing,
+        home: true,
+    })
 
-  return (
+    function handleClickOnAboutUs() {
+        if (visibility.aboutUs) {
+            setVisibility({...nothing, home: true})
+        } else {
+            setVisibility({...nothing, aboutUs: true})
+        }
+    }
 
-    <div className="app">
-      <div className="nav">
-        <button onClick={handleClickOnAboutUs}>About us</button>
-        <button onClick={handleClickOnHelp} >How to help?</button>
-        <button onClick={handleClickOnContact} >Contact</button>
-      </div>
-      <Form />
-      <PureModal
-        className='modal-window'
-        width='730px'
-        header="Contact Us"
-        isOpen={modal}
-        closeButton="close"
-        closeButtonPosition="bottom"
-        onClose={() => {
-          setModal(false);
-          SetHomeVisibility(true)
-          return true;
-        }}
-      >
-        <div className='contact'>{contactIsShown && <Contact></Contact>}</div>
-      </PureModal>
-      {homeIsShown && <Welcome title={title} shortDesc={shortDesc} />}
-      {aboutUsIsShown && <AboutUs></AboutUs>}
-      {helpIsShown && <HowToHelp></HowToHelp>}
-    </div>
-  );
+    function handleClickOnContact() {
+        if (visibility.contact) {
+            setVisibility({...nothing, home: true})
+        } else {
+            setVisibility({...nothing, contact: true, modal: true})
+        }
+    }
+
+    function handleClickOnHelp() {
+        if (visibility.help) {
+            setVisibility({...nothing, home: true})
+        } else {
+            setVisibility({...nothing, help: true})
+        }
+    }
+
+    const handleClickOnChildren = () => setVisibility({...nothing, children: !visibility.children});
+
+    return (
+
+        <div className="app">
+            <div className="nav">
+                <button onClick={handleClickOnAboutUs}>About us</button>
+                <button onClick={handleClickOnHelp}>How to help?</button>
+                <button onClick={handleClickOnContact}>Contact</button>
+            </div>
+            <Form/>
+            <PureModal
+                className='modal-window'
+                width='730px'
+                header="Contact Us"
+                isOpen={visibility.modal}
+                closeButton="close"
+                closeButtonPosition="bottom"
+                onClose={() => {
+                    setVisibility({...nothing, modal: false, home: true})
+                    return true;
+                }}
+            >
+                <div className='contact'>{visibility.contact && <Contact/>}</div>
+            </PureModal>
+            {visibility.home && <Welcome title={title} shortDesc={shortDesc}/>}
+            {visibility.aboutUs && <AboutUs/>}
+            {visibility.help && <HowToHelp handleClickOnChildren={handleClickOnChildren}/>}
+            {visibility.children && <ChildrenCard/>}
+        </div>
+    );
 }
 
 export default App;
